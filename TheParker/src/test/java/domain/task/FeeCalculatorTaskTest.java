d7ffb5c754d4b723c64e3b9d700ce066603bef8e
Juan Sebastián Salazar Aguirre - Ceiba Software <juan.salazar@ceiba.com.co>
2018-01-23 13:38:04 -0500

    Task test added

--- TheParker/src/test/java/domain/task/FeeCalculatorTaskTest.java
+++ TheParker/src/test/java/domain/task/FeeCalculatorTaskTest.java
@@ -3,20 +3,92 @@
  */
 package domain.task;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.Date;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import co.com.ceiba.theparker.domain.manager.AutoUpdateManager;
+import co.com.ceiba.theparker.domain.model.VehicleRecord;
+import co.com.ceiba.theparker.domain.task.impl.FeeCalculatorTaskImpl;
+import co.com.ceiba.theparker.infrastructure.dao.FeeCalculatorTaskDao;
+import co.com.ceiba.theparker.infrastructure.dao.impl.FeeCalculatorTaskDaoImpl;
+import testDataBuilder.VehicleRecordDataBuilder;
 
 /**
  * @author juan.salazar
  *
  */
+@RunWith(MockitoJUnitRunner.class)
 public class FeeCalculatorTaskTest {
-
+	
+	@InjectMocks
+	FeeCalculatorTaskImpl feeCalculatorTaskImpl;
+	
+	@Mock
+	AutoUpdateManager autoUpdateManager;
+	
+	@Mock
+	FeeCalculatorTaskDao feeCalculatorTaskDao;
+	
+	@Before
+	public void setup() {
+		//parkingLotManagerImpl = new ParkingLotManagerImpl();
+		feeCalculatorTaskDao = new FeeCalculatorTaskDaoImpl();
+	}
+	
 	/*
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private void getActiveVehicleRecordTest() {
+		// Arrange
+		List<VehicleRecord> records = new ArrayList<>();
+		VehicleRecord record = new VehicleRecordDataBuilder().build();
+		records.add(record);
+		
+		// Act
+		Mockito.when(feeCalculatorTaskImpl.getActiveVehicleRecord()).thenReturn(records);
+		
+		// Assert
+		assertNotNull(records);
 	}*/
+	
+	@Test
+	public void findParkingTimeTest() {
+		// Arrange
+		Date currentTime = new Date();
+		VehicleRecord record = new VehicleRecordDataBuilder().build();
+		// Act
+		
+		Long time = feeCalculatorTaskImpl.findParkingTime(record);
+		
+		// Assert
+		assertNotNull(time);
+	}
+	
+	@Test
+	public void findParkingHoursTest() {
+		// Arrange
+		Long time = 30000000L;
+		// Act
+		Double hours = feeCalculatorTaskImpl.findParkingHours(time);
+		// Assert
+		assertNotNull(time);
+	}
+	
+	@Test
+	public void findParkingTimeToShowTest() {
+		// Arrange
+		Double hours = 10.5;
+		// Assert
+		String hoursShow = feeCalculatorTaskImpl.findParkingTimeToShow(hours);
+		// Assert
+		assertNotNull(hoursShow);
+	}
 
 }
